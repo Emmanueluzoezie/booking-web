6a576d678646b78496f83cbd429a15758d8a9162
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-01-09 11:06:07 +0100

    adding git ignore file and context file

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b5c13761565deb7"; }
+/******/ 		__webpack_require__.h = function() { return "4000ff9e915209c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
